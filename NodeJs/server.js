dcddc4ca5f0d2b69c3e323ae12ac87bb2ae3118b
Marcelo Machado <m.machado@cigamgestor.com.br>
2020-01-03 13:37:43 -0300

    FInalizando aulas NodeJs

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
@@ -6,6 +7,7 @@ const requireDir = require('require-dir');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // Iniciando o Db
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
